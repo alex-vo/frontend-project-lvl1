d920a869355fa28b4400858a54217d3a62b90a97
Aleksei <avoroncovs@gmail.com>
2021-09-14 18:55:18 +0300

    minor polish brain-progression.js

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -10,7 +10,7 @@ playGame('What number is missing in the progression?', () => {
     const step = Math.floor(Math.random() * number2UpperBound);
     const numberCount = Math.floor(Math.random() * 5) + 5;
     const hiddenNumber = Math.floor(Math.random() * numberCount);
-    let question = ['Question: '];
+    let question = ['Question:'];
     for(let i = 0; i < numberCount; i++) {
         if (i === hiddenNumber) {
             question.push('..');    
