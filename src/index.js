27dee7ee5838ea621ad410a2a68053b175060a11
Aleksei <avoroncovs@gmail.com>
2021-09-17 23:12:19 +0300

    fixed 'questionText' reference in index.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ export default (name, getNextQuestion) => {
   let correctAnswers = 0;
 
   while (correctAnswers < maxCorrectCount) {
-    const { question, correctAnswer } = getNextQuestion();
-    console.log(question);
+    const { questionText, correctAnswer } = getNextQuestion();
+    console.log(questionText);
     const answer = readlineSync.question('Your answer: ');
     if (correctAnswer !== answer.toLowerCase()) {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
