5e784f2c502be3d485259e64ab352c2ce85a6f77
Aleksei <avoroncovs@gmail.com>
2021-09-19 17:46:57 +0300

    got rid of upper bound constants in gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -10,12 +10,9 @@ const gcd = (a, b) => {
   return gcd(b, a % b);
 };
 
-const number1UpperBound = 100;
-const number2UpperBound = 100;
-
 const createNextQuestion = () => {
-  const number1 = generateRandomNumber(0, number1UpperBound);
-  const number2 = generateRandomNumber(0, number2UpperBound);
+  const number1 = generateRandomNumber(0, 100);
+  const number2 = generateRandomNumber(0, 100);
   const correctAnswer = `${gcd(number1, number2)}`;
   const questionText = `${number1} ${number2}`;
 
